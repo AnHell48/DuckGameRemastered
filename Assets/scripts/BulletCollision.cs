e221d96ffcec817032be2b8c27c52b854a4f6e2e
Angel <93478494+ARR48@users.noreply.github.com>
2022-05-13 22:58:23 -0400

    inital

---
+++ Assets/scripts/BulletCollision.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletCollision : MonoBehaviour
+{
+    GameObject invisibleWall;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        invisibleWall = GameObject.Find("InvisibleWall");
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        //Delete bullet if it go behind the wall
+        if(transform.position.z > invisibleWall.transform.position.z)
+        {
+            //Debug.Log("deletion point reached ...");
+            DeleteBullet();
+        }
+    }
+
+    private void OnTriggerEnter(Collider col)
+    {
+        string quack = col.gameObject.name;
+        //Debug.Log("REEEEEE");
+
+        //if the bullet hit a duck then play the sound effect
+        if (quack.Contains("duck"))
+        {
+            //play sound
+            col.gameObject.GetComponent<AudioSource>().Play();
+        }
+        DeleteBullet();
+    }
+
+    private void DeleteBullet()
+    {
+        Destroy(gameObject);
+    }
+}
+
+
---
+++ Assets/scripts/Gun.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gun : MonoBehaviour
+{
+    float gunRotSpeed, gunBounds;
+    List<GameObject> gunList;
+    public GameObject bulletSpawner;
+    Transform bulletSpawnPoint;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        gunList = new List<GameObject>();
+        bulletSpawnPoint = GameObject.Find("bulletSpawnPoint").transform;
+        gunRotSpeed = 1.0f;
+        gunBounds = 0.18f;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        PlayerController();
+        Debug.Log("spawnPoint: "+bulletSpawnPoint.position);
+        Debug.Log("gun: "+transform.localPosition);
+    }
+
+    private void PlayerController()
+    {
+
+        if (Input.GetKey(KeyCode.D) && transform.rotation.y <= gunBounds)
+        {
+            transform.Rotate(new Vector3(0, gunRotSpeed, 0));
+        }
+
+        if (Input.GetKey(KeyCode.A) && transform.rotation.y >= -gunBounds)
+        {
+            transform.Rotate(new Vector3(0, -gunRotSpeed, 0));
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            SpawnBullet();
+            print("pew pew");
+        }
+    }
+
+    private void SpawnBullet()
+    {
+        //GameObject bullet = new GameObject();
+        GameObject bullet = Instantiate(Resources.Load("bullet", typeof(GameObject)), bulletSpawnPoint.position, this.transform.rotation) as GameObject;
+        bullet.GetComponent<Rigidbody>().AddForce(bullet.transform.forward * 20f);
+       // bullet.transform.position = bulletSpawnPoint.position;
+
+    }
+    
+}
